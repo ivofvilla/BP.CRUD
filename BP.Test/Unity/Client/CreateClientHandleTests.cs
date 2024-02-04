7117e4296fb6d2962ab63e4c1fb3975349bf8e57
Ivo Ferreira Villa <ivofvilla@hotmail.com>
2024-02-04 15:21:31 -0300

    testes Create

---
+++ BP.Test/Unity/Client/CreateClientHandleTests.cs
@@ -0,0 +1,54 @@
+﻿using BP.CRUD.Domain.Commands.Client.Create;
+using BP.CRUD.Repository.Interfaces;
+using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+
+namespace BP.Test.Unity.Client
+{
+    public class CreateClientHandleTests
+    {
+        [Fact]
+        public async Task Handle_ValidCommand_ShouldReturnTrue()
+        {
+            // Arrange
+            var clientRepositoryMock = new Mock<IClientRepository>();
+            var validatorMock = new Mock<IValidator<CreateClientCommand>>();
+            validatorMock.Setup(validator => validator.ValidateAsync(It.IsAny<CreateClientCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ValidationResult());
+
+            var createClientHandle = new CreateClientHandle(clientRepositoryMock.Object, validatorMock.Object);
+
+            // Act
+            var result = await createClientHandle.Handle(new CreateClientCommand
+            {
+                Name = "Usuário teste",
+                Email = "usuario@email.com",
+                Phones = new List<PhoneCommand>{
+                            new PhoneCommand { Type = true, DDD = 123, Number = "123456789" },
+                            }
+            }, CancellationToken.None);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task Handle_InvalidCommand_ShouldReturnFalse()
+        {
+            // Arrange
+            var clientRepositoryMock = new Mock<IClientRepository>();
+            var validatorMock = new Mock<IValidator<CreateClientCommand>>();
+            validatorMock.Setup(validator => validator.ValidateAsync(It.IsAny<CreateClientCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ValidationResult(new List<ValidationFailure> { new ValidationFailure("PropertyName", "ErrorMessage") }));
+
+            var createClientHandle = new CreateClientHandle(clientRepositoryMock.Object, validatorMock.Object);
+
+            // Act
+            var result = await createClientHandle.Handle(new CreateClientCommand(), CancellationToken.None);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
